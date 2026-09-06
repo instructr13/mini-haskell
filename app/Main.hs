b244c3c664ece64bbb4e88fa1dc6c013f1adb088
Mido <mido@wycey.dev>
2026-09-07 03:44:40 +0900

    Show final computed main function

--- app/Main.hs
+++ app/Main.hs
@@ -15,4 +15,13 @@ runMain args = do
   case result of
     Left e -> error (show e)
     Right trs -> do
-      putStr (showTRS trs) -- Output a normal form of F "main" [].
+      putStr (showTRS trs)
+
+      -- Output a normal form of F "main" [].
+      let maybeMain = lookup (F "main" []) trs
+
+      case maybeMain of
+        Nothing -> error "No main function found or is invalid"
+        Just mainF -> do
+          putStr "--> "
+          putStrLn (show (nf trs mainF))
